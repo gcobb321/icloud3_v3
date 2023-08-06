683fd02bde55ab1df1c79fd92f9e36b71293b3cc
gcobb321 <geekstergary@gmail.com>
2023-08-06 16:31:08 -0400

    iCloud3 v3, Release Candidate 1

--- custom_components/icloud3/__init__.py
+++ custom_components/icloud3/__init__.py
@@ -130,6 +130,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         config_file.load_storage_icloud3_configuration_file()
         start_ic3.set_log_level(Gb.log_level)
         open_ic3_log_file(new_log_file=Gb.log_debug_flag)
+        # config_file.count_lines_of_code(Gb.icloud3_directory)
 
         Gb.evlog_btnconfig_url = Gb.conf_profile[CONF_EVLOG_BTNCONFIG_URL].strip()
         Gb.evlog_version       = Gb.conf_profile['event_log_version']
